be24938fe068b5f714c57cb479194d18dc4cd3e7
Lewis Russell <lewis6991@gmail.com>
2021-09-03 12:26:40 +0100

    Output profile report to a new buffer instead of :messages

--- lua/impatient/profile.lua
+++ lua/impatient/profile.lua
@@ -1,8 +1,6 @@
 local M = {}
 
-local function printf(msg, ...)
-  print(string.format(msg, ...))
-end
+local api = vim.api
 
 function M.print_profile(profile)
   if not profile then
@@ -12,21 +10,22 @@ function M.print_profile(profile)
 
   local total_resolve = 0
   local total_load = 0
-
+  local name_pad = 0
   local profile_sorted = {}
 
-  local name_pad = 0
-  for k, p in pairs(profile) do
+  for module, p in pairs(profile) do
     p.resolve = p.resolve / 1000000
     p.load    = p.load / 1000000
+    p.total   = p.resolve + p.load
+    p.module  = module
 
     total_resolve = total_resolve + p.resolve
-    total_load = total_load + p.load
-    p.module = k
-    if #k > name_pad then
-      name_pad = #k
+    total_load   = total_load + p.load
+
+    if #module > name_pad then
+      name_pad = #module
     end
-    p.total = p.resolve + p.load
+
     profile_sorted[#profile_sorted+1] = p
   end
 
@@ -34,14 +33,24 @@ function M.print_profile(profile)
     return a.total > b.total
   end)
 
-  printf('%-'..name_pad..'s │ Resolve    │ Load       │ Total      │', 'Module')
-  printf('%s │ ---------- │ ---------- │ ---------- │', string.rep('-', name_pad))
-  for _, p in pairs(profile_sorted) do
-    printf('%-'..name_pad..'s │ %8.4fms │ %8.4fms │ %8.4fms │', p.module, p.resolve, p.load, p.total)
+  local lines = {}
+  local function add(...)
+    lines[#lines+1] = string.format(...)
   end
-  printf('%s │ ---------- │ ---------- │ ---------- │', string.rep('-', name_pad))
-  printf('%-'..name_pad..'s │ %8.4fms │ %8.4fms │ %8.4fms │', 'Total', total_resolve, total_load, total_resolve+total_load)
 
+  add('%-'..name_pad..'s │ Resolve    │ Load       │ Total      │', 'Module')
+  add('%s │ ---------- │ ---------- │ ---------- │', string.rep('-', name_pad))
+  add('%-'..name_pad..'s │ %8.4fms │ %8.4fms │ %8.4fms │', 'Total', total_resolve, total_load, total_resolve+total_load)
+  add('%s │ ---------- │ ---------- │ ---------- │', string.rep('-', name_pad))
+  for _, p in pairs(profile_sorted) do
+    add('%-'..name_pad..'s │ %8.4fms │ %8.4fms │ %8.4fms │', p.module, p.resolve, p.load, p.total)
+  end
+
+  local bufnr = api.nvim_create_buf(false, false)
+  api.nvim_buf_set_lines(bufnr, 0, 0, false, lines)
+  api.nvim_buf_set_option(bufnr, 'buftype', 'nofile')
+  api.nvim_buf_set_name(bufnr, 'Impatient Profile Report')
+  api.nvim_set_current_buf(bufnr)
 end
 
 return M
