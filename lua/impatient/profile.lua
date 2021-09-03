5e1102376593637c4e1d248ddcd6f72178e71baf
Lewis Russell <lewis6991@gmail.com>
2021-09-03 17:49:12 +0100

    profile: report times of plugins and modules

--- lua/impatient/profile.lua
+++ lua/impatient/profile.lua
@@ -11,13 +11,37 @@ function M.print_profile(profile)
   local total_resolve = 0
   local total_load = 0
   local name_pad = 0
-  local profile_sorted = {}
+  local modules = {}
+  local plugins = {}
 
   for module, p in pairs(profile) do
     p.resolve = p.resolve / 1000000
     p.load    = p.load / 1000000
     p.total   = p.resolve + p.load
-    p.module  = module
+    p.module  = module:gsub('/', '.')
+
+    local plugin = p.module:match('([^.]+)')
+    if plugin then
+      if not plugins[plugin] then
+        plugins[plugin] = {
+          module = plugin,
+          resolve = 0,
+          load = 0,
+          total = 0
+        }
+      end
+      local r = plugins[plugin]
+
+      r.resolve = r.resolve + p.resolve
+      r.load    = r.load + p.load
+      r.total   = r.total + p.total
+
+      if not r.loader then
+        r.loader = p.loader
+      elseif r.loader ~= p.loader then
+        r.loader = 'mixed'
+      end
+    end
 
     total_resolve = total_resolve + p.resolve
     total_load   = total_load + p.load
@@ -26,10 +50,22 @@ function M.print_profile(profile)
       name_pad = #module
     end
 
-    profile_sorted[#profile_sorted+1] = p
+    modules[#modules+1] = p
   end
 
-  table.sort(profile_sorted, function(a, b)
+  table.sort(modules, function(a, b)
+    return a.module > b.module
+  end)
+
+  do
+    local plugins_a = {}
+    for _, v in pairs(plugins) do
+      plugins_a[#plugins_a+1] = v
+    end
+    plugins = plugins_a
+  end
+
+  table.sort(plugins, function(a, b)
     return a.total > b.total
   end)
 
@@ -38,13 +74,25 @@ function M.print_profile(profile)
     lines[#lines+1] = string.format(...)
   end
 
-  add('%-'..name_pad..'s │ Loader    │ Resolve    │ Load       │ Total      │', 'Module')
-  add('%s┼───────────┼────────────┼────────────┼────────────┤', string.rep('─', name_pad+1))
+  local l = string.rep('─', name_pad+1)
+
+  add('%s┬───────────┬────────────┬────────────┬────────────┐', l)
+  add('%-'..name_pad..'s │ Loader    │ Resolve    │ Load       │ Total      │', '')
+  add('%s┼───────────┼────────────┼────────────┼────────────┤', l)
   add('%-'..name_pad..'s │           │ %8.4fms │ %8.4fms │ %8.4fms │', 'Total', total_resolve, total_load, total_resolve+total_load)
-  add('%s┼───────────┼────────────┼────────────┼────────────┤', string.rep('─', name_pad+1))
-  for _, p in pairs(profile_sorted) do
+  add('%s┴───────────┴────────────┴────────────┴────────────┤', l)
+  add('%-'..name_pad..'s                                                    │', 'By Plugin')
+  add('%s┬───────────┬────────────┬────────────┬────────────┤', l)
+  for _, p in ipairs(plugins) do
     add('%-'..name_pad..'s │ %9s │ %8.4fms │ %8.4fms │ %8.4fms │', p.module, p.loader, p.resolve, p.load, p.total)
   end
+  add('%s┴───────────┴────────────┴────────────┴────────────┤', l)
+  add('%-'..name_pad..'s                                                    │', 'By Module')
+  add('%s┬───────────┬────────────┬────────────┬────────────┤', l)
+  for _, p in pairs(modules) do
+    add('%-'..name_pad..'s │ %9s │ %8.4fms │ %8.4fms │ %8.4fms │', p.module, p.loader, p.resolve, p.load, p.total)
+  end
+  add('%s┴───────────┴────────────┴────────────┴────────────┘', l)
 
   local bufnr = api.nvim_create_buf(false, false)
   api.nvim_buf_set_lines(bufnr, 0, 0, false, lines)
