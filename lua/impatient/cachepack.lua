83571d493e3f517e89783296c9ff410f030ea211
Lewis Russell <lewis6991@gmail.com>
2021-09-06 16:26:08 +0100

    use less tables

--- lua/impatient/cachepack.lua
+++ lua/impatient/cachepack.lua
@@ -6,26 +6,16 @@ local M = {}
 local c_double = ffi.typeof("double[1]")
 local sizeof_c_double = ffi.sizeof("double")
 
-local OutputBuffer = {}
-
-function OutputBuffer.create() return {} end
-
-function OutputBuffer.write_number(buf, num)
+local function write_number(buf, num)
   buf[#buf+1] = ffi.string(c_double(num), sizeof_c_double)
 end
 
-function OutputBuffer.write_string(buf, str)
-  OutputBuffer.write_number(buf, #str)
+local function write_string(buf, str)
+  write_number(buf, #str)
   buf[#buf+1] = str
 end
 
-function OutputBuffer.to_string(buf)
-  return table.concat(buf)
-end
-
-local InputBuffer = {}
-
-function InputBuffer.create(str)
+function create_input_buffer(str)
   return {
     ptr = ffi.new("const char[?]", #str, str),
     pos = 0,
@@ -33,15 +23,15 @@ function InputBuffer.create(str)
   }
 end
 
-function InputBuffer.read_number(buf)
+function read_number(buf)
   if (buf.size < buf.pos) then error("buffer access violation") end
   local res = ffi.cast("double*", buf.ptr + buf.pos)[0]
   buf.pos = buf.pos + sizeof_c_double
   return res
 end
 
-function InputBuffer.read_string(buf)
-  local len = InputBuffer.read_number(buf)
+function read_string(buf)
+  local len = read_number(buf)
   local res = ffi.string(buf.ptr + buf.pos, len)
   buf.pos = buf.pos + len
 
@@ -50,17 +40,17 @@ end
 
 function M.pack(cache)
   local total_keys = vim.tbl_count(cache)
-  local buf = OutputBuffer.create()
+  local buf = {}
 
-  OutputBuffer.write_number(buf, total_keys)
+  write_number(buf, total_keys)
   for k,v in pairs(cache) do
-    OutputBuffer.write_string(buf, k)
-    OutputBuffer.write_string(buf, v[1] or "")
-    OutputBuffer.write_number(buf, v[2] or 0)
-    OutputBuffer.write_string(buf, v[3] or "")
+    write_string(buf, k)
+    write_string(buf, v[1] or "")
+    write_number(buf, v[2] or 0)
+    write_string(buf, v[3] or "")
   end
 
-  return OutputBuffer.to_string(buf)
+  return table.concat(buf)
 end
 
 function M.unpack(str)
@@ -68,18 +58,17 @@ function M.unpack(str)
     return {}
   end
 
-  local buf = InputBuffer.create(str)
+  local buf = create_input_buffer(str)
   local cache = {}
 
-  local total_keys = InputBuffer.read_number(buf)
+  local total_keys = read_number(buf)
   for _ = 1, total_keys do
-    local k = InputBuffer.read_string(buf)
-    local v = {
-      InputBuffer.read_string(buf),
-      InputBuffer.read_number(buf),
-      InputBuffer.read_string(buf)
+    local k = read_string(buf)
+    cache[k] = {
+      read_string(buf),
+      read_number(buf),
+      read_string(buf)
     }
-    cache[k] = v
   end
 
   return cache
